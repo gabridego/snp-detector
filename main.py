8fe81afa7d35b1e9de93a2dd5f5cb02b6e1dd207
gabridego <superbrio97@gmail.com>
2021-10-25 17:03:17 +0200

    add interface to store/save kmers, update readme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import pickle
 import time
 from typing import Dict
 
@@ -10,10 +11,21 @@ from utils import plot_frequency
 
 def parse_args():
     parser = argparse.ArgumentParser(description="SNP detector")
-    parser.add_argument('-p', '--path', nargs=2, help='Paths to FASTA files')
+    parser.add_argument('-p', '--path', nargs=2, help='Paths to FASTA files (or to stored binary files if -l)')
     parser.add_argument('-k', type=int, nargs='?', default=20, help='Length of patterns')
+    parser.add_argument('-t', '--threshold', type=int, nargs='?', default=1, help='Threshold for K-mers filtering')
     parser.add_argument('-v', '--visualize', default=False, action='store_true', help='Plot intermediate results')
+    parser.add_argument('-s', '--save', default=False, action='store_true', help='Save collected kmers')
+    parser.add_argument('-l', '--load', default=False, action='store_true', help='Load collected kmers')
     return parser.parse_args()
+
+
+def verify_args(args):
+    if args.save and args.load:
+        raise ValueError("-s and -l cannot be used at the same time")
+
+    if args.load and (args.path[0].endswith('.fna') or args.path[1].endswith('.fna')):
+        raise ValueError("in load mode, program expects binary files, not FASTA")
 
 
 def parse_kmers(filename: str, k: int):
@@ -36,7 +48,7 @@ def parse_kmers(filename: str, k: int):
     return kmers
 
 
-def filter_kmers(kmers: Dict[str, int], threshold=1):
+def filter_kmers(kmers: Dict[str, int], threshold: int = 1):
     filtered_kmers = {k: v for k, v in kmers.items() if v > threshold}
     kmers.clear()
     kmers.update(filtered_kmers)
@@ -50,33 +62,52 @@ def main(args):
     k = args.k
 
     # read and filter wild strain
-    wild_kmers = parse_kmers(args.path[0], k)
+    if args.load:
+        print(f"Load wild kmers from {args.path[0]}...")
+        with open(args.path[0], 'rb') as f:
+            wild_kmers = pickle.load(f)
+    else:
+        wild_kmers = parse_kmers(args.path[0], k)
+        if args.save:
+            filename = os.path.splitext(args.path[0])[0] + '_' + str(args.k) + '.pickle'
+            print(f"Store wild kmers in {filename}...")
+            with open(filename, 'wb') as f:
+                pickle.dump(wild_kmers, f)
 
     if args.visualize:
         plot_frequency(wild_kmers, "Distribution of K-mers' number of occurrences for wild strain")
 
-    filter_kmers(wild_kmers)
+    filter_kmers(wild_kmers, args.threshold)
 
     if args.visualize:
         plot_frequency(wild_kmers, "Distribution of K-mers' number of occurrences for wild strain,"
                                    "after error filtering")
 
     # read and filter mutated strain
-    mut_kmers = parse_kmers(args.path[1], k)
+    if args.load:
+        print(f"Load mutated kmers from {args.path[1]}...")
+        with open(args.path[1], 'rb') as f:
+            mut_kmers = pickle.load(f)
+    else:
+        mut_kmers = parse_kmers(args.path[1], k)
+        if args.save:
+            filename = os.path.splitext(args.path[1])[0] + '_' + str(args.k) + '.pickle'
+            print(f"Store mutated kmers in {filename}...")
+            with open(filename, 'wb') as f:
+                pickle.dump(mut_kmers, f)
 
     if args.visualize:
         plot_frequency(mut_kmers, "Distribution of K-mers' number of occurrences for mutated strain")
 
-    filter_kmers(mut_kmers)
+    filter_kmers(mut_kmers, args.threshold)
 
     if args.visualize:
         plot_frequency(mut_kmers, "Distribution of K-mers' number of occurrences for mutated strain,"
                                   "after error filtering")
 
-    # compare the k-mers collected for the two strains
-
 
 if __name__ == '__main__':
     args = parse_args()
+    verify_args(args)
 
     main(args)
